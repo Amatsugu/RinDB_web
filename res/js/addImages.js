9c5a676d44610e2fc1cc1ebb3d435d3d91fbd6fa
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2016-11-04 00:27:41 -0400

    Names based on Lorem ipsum

--- res/js/addImages.js
+++ res/js/addImages.js
@@ -2,10 +2,11 @@ $(document).ready(function()
 {
 	var card = $(".imageCard");
 	var img;
+	var names = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua".split(" ");
 	for(var i = 0; i < 100; i ++)
 	{
 		img = card.clone();
-		img.appendTo("#contentGrid").children(".name").text("Image " + i);
+		img.appendTo("#contentGrid").children(".name").text(names[i%names.length]);
 		img.find("img").attr("src", "res/img/imgtest"+((i%14)+1)+".jpg");
 	}
 	var item = $(".inputArea");
