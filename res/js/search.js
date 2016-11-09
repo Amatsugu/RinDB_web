fe77d9bbb3e66d49a51cdfd81c964f943501e304
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2016-11-09 18:03:23 -0500

    Added Ajax data retrevial and templating

--- res/js/search.js
+++ res/js/search.js
@@ -1,12 +1,78 @@
 var search;
 var content;
+var curReq = "/DB/latest/";
+var count = 25;
+var page = 1;
+var lastReq;
 
 $(document).ready(function()
 {
 	search = $("#search");
 	content = $("#contentGrid");
+	lastReq = $.ajax(
+	{
+		url:curReq+count+"/"+page,
+		async: false,
+		success: function(text)
+		{
+			content.append(text);
+			$(".imageCard").fadeIn();
+		}
+	});
+	$(window).scroll(function() 
+	{
+   		if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) 
+		{
+			page++;
+			console.log(page);
+       		lastReq = $.ajax(
+			{
+				url:curReq+count+"/"+page,
+				async: true,
+				success: function(text)
+				{
+					content.append(text);
+					$(".imageCard").fadeIn();
+				}
+			});
+	   	}
+	});
 	//Seach Feild
-	search.on("propertychange change click keyup input paste submit", Search);
+	var lastQuery;
+	search.on("propertychange change keyup input paste submit", function (event)
+	{
+		event.preventDefault();
+		var query = $("input[type=search]").val();
+		if(lastQuery != query)
+		{
+			lastQuery = query;
+			console.log(query);
+			page = 1;
+			if(lastReq && lastReq.readyState != 4)
+				lastReq.abort();
+			if(query == "")
+			{
+				curReq = "/DB/latest/";
+			}else
+			{
+				curReq = "/DB/search/"+query+"/";
+			}
+			content.html("<div class=\"searching\">Searching...</div");
+			var curPage;
+			lastReq = $.ajax(
+			{
+				url:curReq+count+"/"+page,
+				async: true,
+				success: function(text)
+				{
+					if(text == "")
+						text = "<div class=\"searching\">No Results found</div";
+					content.html(text);
+					$(".imageCard").fadeIn();
+				}
+			});
+		}
+	});
 	//Advanced Search
 	var arrow = $("#advancedSearchButton");
 	arrow.on("click", function()
@@ -24,16 +90,16 @@ $(document).ready(function()
 	});
 });
 
-function Search(event)
+
+
+function GetMatchingTags(query)
 {
-	event.preventDefault();
-	var query = $("input[type=search]").val();
-	var cards = content.find(".imageCard");
-	if(query.length == 0)
+	var tags = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua".toLocaleLowerCase().split(" ");
+	var outTags;
+	for(var i = 0; i < tags.length; i++)
 	{
-		cards.fadeIn();
-		return;
+		if(tags[i].search(query.toLocaleLowerCase()))
+			outTags += tags[i] + "|";
 	}
-	cards.hide();
-	cards.find("div:contains('"+query+"')").parent().show();
+	return outTags.split("|");
 }
